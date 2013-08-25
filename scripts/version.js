0de5b2d8fd775ec538c92b2c3db83a08f5c2074c
Susam Pal <susam@susam.in>
2013-08-25 15:49:16 +0530

    Update version to 1.0-beta1
    Set Version.VERSION = '1.0-beta1'.

--- scripts/version.js
+++ scripts/version.js
@@ -31,7 +31,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 var Version = function()
 {
     // Version of this application
-    var VERSION = '1.0-alpha'
+    var VERSION = '1.0-beta1'
 
 
     // Display this version number in an HTML element with its id as
